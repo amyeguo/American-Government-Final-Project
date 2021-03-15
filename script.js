7c36a8c0ba2e0ee6eaf9a50bee286838ee1df89e
Amy Guo <72362221+amyeguo@users.noreply.github.com>
2021-03-15 15:57:42 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 var typed = new Typed("#typed", {
   stringsElement: "#typed-strings",
-  typeSpeed: 0,
+  typeSpeed: 20,
 });
-
-var typed = new Typed("#typed2", {
-  stringsElement: "#fasttyped",
-  typeSpeed: 0,
-});
-
-const primaryButton = document.querySelector("primary");
-
-primaryButton.addEventListener("click", howPrimary());
-function howPrimary() {
-  console.log("HELLO");
-  /*let oldText = document.getElementById("typed-strings");
-  oldText.innerHTML = `<p> How did you win the primary? </p>`;
-  console.log(oldText);*/
-}
